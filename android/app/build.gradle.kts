4f04f71ba264e5eb03bd1cc935add9a6e50889e1
mehrabrafi <mehrabalrafipersonal@gmail.com>
2025-04-16 12:47:02 +0600

    minimum android version set

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,13 +1,12 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+    id("dev.flutter.flutter-gradle-plugin") // Flutter plugin must be applied last
 }
 
 android {
     namespace = "com.example.pathagar"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34 // Correctly set outside defaultConfig
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -20,20 +19,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.pathagar"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21 // Corrected
+        targetSdk = 34 // Corrected
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
